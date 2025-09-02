089196834ed332d9b0daf64ef31234e9488b9730
Korede Anunusor <koredeanunusor@gmail.com>
2025-09-02 12:16:48 +0100

    probability

--- DataScience/probability.py
+++ DataScience/probability.py
@@ -4,4 +4,15 @@ purchases = np.array([10, 20, 30, 200, 300])
 
 mean_value = purchases.mean()
 
-print(mean_value)
+# print(mean_value)
+
+salary = np.array([500, 600, 700, 800, 1000, 10000, 50000 ])
+
+# print(salary.mean())
+
+data = np.array([45, 50, 52, 55, 60, 65,150, 200])
+
+average = data.mean()
+middle = np.median(data)
+print(middle, average)
+print(f"Mean: {average}\nMedian: {middle}")
